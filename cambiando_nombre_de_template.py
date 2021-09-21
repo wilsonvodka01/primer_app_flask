3310cf2d127c89131624598c7289e167d2e5f76c
Alexander <wilgm0kdev@gmail.com>
2021-09-21 11:58:21 +0200

    usando template tags

--- cambiando_nombre_de_template.py
+++ cambiando_nombre_de_template.py
@@ -2,12 +2,11 @@
 # -*- coding: utf-8 -*-
 from flask import Flask
 from flask import render_template
-app = Flask(__name__)
+app = Flask(__name__, template_folder = "prueba_template")
 
-@app.route('/user/')
-@app.route('/user/<name>')
-def user(name='alexander'):
-  return render_template('index.html', nombre=name)
+@app.route('/')
+def index():
+  return render_template('index.html')
 
 if __name__ == '__main__':
   app.run(debug = True, port= 8001)
