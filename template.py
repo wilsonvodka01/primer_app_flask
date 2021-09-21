f8c7491f4d0ddf89ccb0967fe2d36b801bb3dda3
Alexander <wilgm0kdev@gmail.com>
2021-09-21 11:42:17 +0200

    cmabiando la carpeta por defecto de template

--- template.py
+++ template.py
@@ -1,12 +1,10 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
 from flask import Flask
 from flask import render_template
-app = Flask(__name__, template_folder = "prueba_template")
+app = Flask(__name__)
 
 @app.route('/')
 def index():
   return render_template('index.html')
-
+  
 if __name__ == '__main__':
-  app.run(debug = True, port= 8001)
+  app.run(debug = True, port= 5050)
