8fa97c728cd929ef5c399dbc38ab940f5a649622
Alexander <wilgm0kdev@gmail.com>
2021-09-21 12:45:40 +0200

    herencia en templates usando rutas adicionales

--- app.py
+++ app.py
@@ -10,6 +10,10 @@ def user(name='alexander'):
   age = 19
   my_list = [1,2,3,4]
   return render_template('index.html', nombre=name, age = age, list = my_list)
+@app.route('/client')
+def client():
+  list_name = ['test1','test2','test3']
+  return render_template('client.html',list = list_name )
 
 if __name__ == '__main__':
   app.run(debug = True, port= 5000)
